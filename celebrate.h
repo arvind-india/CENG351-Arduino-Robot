abc57ecbbc7f47cde68baeb8255d21f9ca471258
7385302 <Walter.Vaughan@mines.sdsmt.edu>
2016-11-28 16:46:10 -0700

    fixed Grace's code >:(

--- celebrate.h
+++ celebrate.h
@@ -1,12 +1,47 @@
 #ifndef CELEBRATION_PIN
 #define CELEBRATION_PIN 12
-#define ONCE (200)
 #endif
+
+#define ONCE (200)
+
 
 void celebrate_setup() {
   pinMode(CELEBRATION_PIN, OUTPUT);
   digitalWrite(CELEBRATION_PIN, LOW);
 }
+
+void on(){
+  digitalWrite(CELEBRATION_PIN, HIGH);
+}
+void off(){
+  digitalWrite(CELEBRATION_PIN, LOW);
+}
+
+void dot(){
+  on();
+  delay(ONCE);
+}
+
+void dash(){
+  on();
+  delay(ONCE*3);
+}
+
+void next_thing(){
+  off();
+  delay(ONCE);
+}
+
+void next_letter(){
+  off();
+  delay(ONCE*3);
+}
+
+void next_word(){
+  off();
+  delay(ONCE*7);
+}
+
 
 void celebrate() {
 
@@ -37,36 +72,3 @@ void celebrate() {
   next_word(); 
   
 }
-
-void on(){
-  dititalWrite(LED, HIGH);
-}
-void off(){
-  dititalWrite(LED, LOW);
-}
-
-void dot(){
-  on();
-  delay(ONCE);
-}
-
-void dash(){
-  on();
-  delay(ONCE*3);
-}
-
-void next_thing(){
-  off();
-  delay(ONCE);
-}
-
-void next_letter(){
-  off();
-  delay(ONCE*3);
-}
-
-void next_word(){
-  off();
-  delay(ONCE*7);
-}
-
